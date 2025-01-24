3e71da44422f7a39a4239dfe6faf17046b2d97df
David <davit.barsamyan@mail.utoronto.ca>
2025-01-24 15:42:13 -0500

    Implement json file to hide answers

--- Barsamyan-D/multiple_choice.py
+++ Barsamyan-D/multiple_choice.py
@@ -20,6 +20,7 @@ def _(mo):
 def _():
     import anywidget
     import traitlets
+    import json
 
     class MultipleChoiceWidget(anywidget.AnyWidget):
         _esm = "mcq.js"
@@ -27,23 +28,28 @@ def _():
 
         question = traitlets.Unicode().tag(sync=True)
         choices = traitlets.List().tag(sync=True)
-        answer = traitlets.Int().tag(sync=True)
+        _answer = traitlets.Int().tag(sync=False)
+        question_id = traitlets.Unicode().tag(sync=False)
         selected = traitlets.Int(-1).tag(sync=True)
         submitted = traitlets.Bool(False).tag(sync=True)
         instruction = traitlets.Unicode().tag(sync=True)
         is_correct = traitlets.Bool(False).tag(sync=True)
 
         def __init__(self, **kwargs):
+            with open("answers.json") as f:
+                answers = json.load(f)
+            kwargs["_answer"] = answers[kwargs["question_id"]]
             super().__init__(**kwargs)
             self.observe(self._validate_answer, names=["selected"])
 
         def _validate_answer(self, change):
-            self.is_correct = (self.selected == self.answer)
+            self.is_correct = (self.selected == self._answer)
 
     q1 = MultipleChoiceWidget(
+        question_id="chemical_symbol_gold",
         question="What is the chemical symbol for gold?",
         choices=["Fe", "Ag", "Au", "Pb"],
-        answer=2,
+        #answer=2,
         instruction="Select an answer and click Submit"
     )
 
