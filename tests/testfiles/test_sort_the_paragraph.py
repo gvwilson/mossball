9399603b64fba52ad14281d2b4365f80c97e0c7e
eun-chae-s <s9710173@naver.com>
2025-03-29 22:45:44 -0400

    add the drag and dropdown button tests

--- tests/testfiles/test_sort_the_paragraph.py
+++ tests/testfiles/test_sort_the_paragraph.py
@@ -51,3 +51,122 @@ def test_basic_structure(get_chrome_driver, start_marimo, mock_server):
     process.terminate()
     process.wait()
     get_chrome_driver.quit()
+
+
+@pytest.mark.parametrize("start_marimo", ["tests/notebooks/sort_the_paragraph.py"], indirect=True)
+def test_drag_paragraphs(get_chrome_driver, start_marimo, mock_server):
+    url, process = start_marimo
+    url = url.encode('ascii', 'ignore').decode('unicode_escape').strip()
+    get_chrome_driver.get(url)
+
+    WebDriverWait(get_chrome_driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "marimo-anywidget")))
+    shadow_hosts = get_chrome_driver.find_elements(By.CSS_SELECTOR, "marimo-anywidget")
+    assert len(shadow_hosts) == 2
+
+    for shadow_host in shadow_hosts:
+        marimo_root = shadow_host.shadow_root
+        widget = marimo_root.find_element(By.CLASS_NAME, "stp")
+
+        form = widget.find_element(By.CLASS_NAME, "main-container")
+        assert form.is_displayed()
+
+        paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        n_paragraphs = len(paragraphs)
+        assert n_paragraphs > 0
+
+        paragraphs_text = [para.get_attribute("data-text") for para in paragraphs]
+        print(paragraphs_text)
+
+        first_paragraph = paragraphs[0]
+        next_paragraph = paragraphs[n_paragraphs - 1]
+
+        # drag down
+        ActionChains(get_chrome_driver).drag_and_drop(first_paragraph, next_paragraph).perform() # drop position = start of the next paragraph
+
+        new_paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        new_paragraphs_text = [para.get_attribute("data-text") for para in new_paragraphs]
+
+        print(new_paragraphs_text)
+        
+        expected_paragraphs_text = paragraphs_text[1:n_paragraphs - 1] + [paragraphs_text[0], paragraphs_text[-1]]
+        assert expected_paragraphs_text == new_paragraphs_text
+
+        # drag up
+        ActionChains(get_chrome_driver).drag_and_drop(new_paragraphs[n_paragraphs - 1], new_paragraphs[1]).perform() # drop position = start of the next paragraph
+
+        expected_paragraphs_text = [new_paragraphs_text[0], new_paragraphs_text[n_paragraphs - 1]] + new_paragraphs_text[1:n_paragraphs - 1]
+
+        new_paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        new_paragraphs_text = [para.get_attribute("data-text") for para in new_paragraphs]
+        print(new_paragraphs_text, expected_paragraphs_text)
+
+        assert expected_paragraphs_text == new_paragraphs_text
+    
+    process.terminate()
+    process.wait()
+    get_chrome_driver.quit()
+
+@pytest.mark.parametrize("start_marimo", ["tests/notebooks/sort_the_paragraph.py"], indirect=True)
+def test_dropdown_arrows(get_chrome_driver, start_marimo, mock_server):
+    url, process = start_marimo
+    url = url.encode('ascii', 'ignore').decode('unicode_escape').strip()
+    get_chrome_driver.get(url)
+
+    WebDriverWait(get_chrome_driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "marimo-anywidget")))
+    shadow_hosts = get_chrome_driver.find_elements(By.CSS_SELECTOR, "marimo-anywidget")
+    assert len(shadow_hosts) == 2
+
+    for shadow_host in shadow_hosts:
+        marimo_root = shadow_host.shadow_root
+        widget = marimo_root.find_element(By.CLASS_NAME, "stp")
+
+        form = widget.find_element(By.CLASS_NAME, "main-container")
+        assert form.is_displayed()
+
+        paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        n_paragraphs = len(paragraphs)
+        assert n_paragraphs > 0
+
+        initial_paragraphs_text = [para.get_attribute("data-text") for para in paragraphs] # text1, text2, text3, text4
+        print(initial_paragraphs_text)
+
+        first_paragraph = paragraphs[0]
+
+        # click dropdown arrow
+        first_paragraph.find_element(By.CLASS_NAME, "arrow-button").click()
+        option_list = first_paragraph.find_element(By.CLASS_NAME, "option-list")
+        assert option_list.is_displayed()
+
+        # move up
+        option_list.find_elements(By.CLASS_NAME, "option")[2].click() # click text3 to move it to the 1st place
+        
+        new_paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        new_paragraphs_text = [para.get_attribute("data-text") for para in new_paragraphs]
+
+        expected_paragraphs_text = [initial_paragraphs_text[2]] + [initial_paragraphs_text[0], initial_paragraphs_text[1]] + initial_paragraphs_text[3:]
+        print(new_paragraphs_text, expected_paragraphs_text)
+        
+        assert expected_paragraphs_text == new_paragraphs_text
+
+        # move down
+        last_paragraph = paragraphs[-1]
+
+        # click dropdown arrow
+        last_paragraph.find_element(By.CLASS_NAME, "arrow-button").click()
+        option_list = last_paragraph.find_element(By.CLASS_NAME, "option-list")
+        assert option_list.is_displayed()
+
+        # move down
+        option_list.find_elements(By.CLASS_NAME, "option")[0].click() # click text1 to move it to the last place (which is currently located in 2nd place)
+        
+        expected_paragraphs_text = [new_paragraphs_text[0]] + new_paragraphs_text[2:] + [new_paragraphs_text[1]]
+        
+        new_paragraphs = form.find_element(By.CLASS_NAME, "texts-container").find_elements(By.CLASS_NAME, "container")
+        new_paragraphs_text = [para.get_attribute("data-text") for para in new_paragraphs]
+        print(new_paragraphs_text, expected_paragraphs_text)
+        
+        assert expected_paragraphs_text == new_paragraphs_text
+    
+    process.terminate()
+    process.wait()
+    get_chrome_driver.quit()
